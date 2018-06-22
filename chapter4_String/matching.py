8672139532c876ef86bf65bee3f54bc9e67b5f3b
zhe <1655604975@qq.com>
2018-06-22 17:31:15 +0800

    'kmp算法,还没看懂T_Y'

--- chapter4_String/matching.py
+++ chapter4_String/matching.py
@@ -20,3 +20,39 @@ def naive_matching(t, p):
             result['count'] += 1
             result['index'].append(i)
     return result
+
+
+def matching_KMP(t, p):
+    n, m = len(t), len(p)
+    i, k = 0, -1
+    pnext = [-1] * m                   # 初始数组全为-1
+    while i < m - 1:                   # 生成下一个pnext元素值
+        if k == -1 or p[i] == p[k]:
+            i, k = i + 1, k + 1
+            pnext[i] = k               # 设置pnext元素
+        else:
+            k = pnext[k]               # 退到更短相同前缀
+
+    i, j = 0, 0
+
+    '''
+    # 展开
+    while j < n and i < m:  # i==m找到匹配
+        if i == -1:                    # 遇到-1,比较下一对字符
+            j, i = j + 1, i + 1
+        elif t[j] == p[i]:             # 字符相等,比较下一对字符
+            j, i = j + 1, i + 1
+        else:
+            i = pnext[i]               # 匹配失败,从pnext取得p的下一字符位置
+    '''
+
+    # 合并if语句
+    while j < n and i < m:
+        if i == -1 or t[j] == p[i]:
+            j, i = j + 1, i + 1
+        else:
+            i = pnext[i]
+    if i == m:                         # 匹配成功,返回下标
+        # return j - 1                 # 返回匹配末尾下标
+        return j - m                   # 返回匹配第一个字符下标
+    return -1                          # 没有找到匹配
