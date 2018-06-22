601bad1d604f723d7e468890afeda48cd55662d2
zhe <1655604975@qq.com>
2018-06-22 10:36:17 +0800

    '循环单链表解josephus'

--- chapter3/Josephue.py
+++ chapter3/Josephue.py
@@ -5,7 +5,7 @@
 # @FileName : Josephue.py
 # @Project  : PyCharm
 
-from .CList import *
+from .CList import LCList
 
 
 def josephus_A(n, k, m):
@@ -20,10 +20,11 @@ def josephus_A(n, k, m):
                 print(people[i], end='')
                 people[i] = 0
             i = (i + 1) % n
-        if num == n-1:
+        if num == n - 1:
             print('')
         else:
             print(', ', end='')
+
 
 def josephus_B(n, k, m):
     people = list(range(1, n + 1))
@@ -33,3 +34,26 @@ def josephus_B(n, k, m):
         i = (i + m - 1) % length
         num = people.pop(i)
         print(num, end=(', ' if length != 1 else '\n'))
+
+
+class Josephus(LCList):
+
+    def __init__(self, n, k, m):
+        super(Josephus, self).__init__()
+        self.n = n
+        self.k = k
+        self.m = m
+        for i in range(1, self.n + 1):
+            self.append(i)
+
+    def run(self):
+        for i in range(self.k-1):
+            self._rear = self._rear.get_next()
+        count = 1
+        while not self.is_empty():
+            self._rear = self._rear.get_next()
+            count += 1
+            if count == self.m:
+                count = 1
+                num = self.pop()
+                print(num.get_elem(), end=(', ' if self.length() != 0 else '\n'))
