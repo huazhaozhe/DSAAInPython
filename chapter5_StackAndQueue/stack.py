6ae11cacc942a884d5301047064b73b4ad4b97a0
zhe <1655604975@qq.com>
2018-07-02 22:03:07 +0800

    背包问题递归求解
    需要将其翻译为非递归求解

--- chapter5_StackAndQueue/stack.py
+++ chapter5_StackAndQueue/stack.py
@@ -160,3 +160,16 @@ def norec_fact(n):
         res *= st.pop()
     return res
 
+
+def knap_rec(weight, wlist, n):
+    if weight == 0:
+        return True
+    if weight < 0 or (weight > 0 and n < 1):
+        return False
+    if knap_rec(weight - wlist[n-1], wlist, n-1):
+        print('Item ' + str(n) + ':', wlist[n-1])
+        return True
+    if knap_rec(weight, wlist, n-1):
+        return True
+    else:
+        return False
