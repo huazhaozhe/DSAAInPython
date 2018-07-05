3ac9beea3bcc24e724943889a6ac3b064ad0eb0f
zhe <1655604975@qq.com>
2018-07-05 15:44:35 +0800

    二叉树的应用:表达式树
    二元表达式,根据先根、后根、中根序列遍历分别得到前缀、后缀和中缀表达式
    使用三元的tuple构造表达式实现
    实现表达式的求值

--- chapter6_BinaryTree/binary_tree.py
+++ chapter6_BinaryTree/binary_tree.py
@@ -30,3 +30,76 @@ def set_left(btree, left):
 
 def set_right(btree, right):
     btree[2] = right
+
+
+def make_sum(a, b):
+    return ('+', a, b)
+
+def make_diff(a, b):
+    return ('-', a, b)
+
+def make_prod(a, b):
+    return ('*', a, b)
+
+def make_div(a, b):
+    return ('/', a, b)
+
+def is_basic_exp(a):
+    return not isinstance(a, tuple)
+
+def is_number(x):
+    return isinstance(x, int) or isinstance(x, float) or isinstance(x, complex)
+
+
+def eval_sum(a, b):
+    if is_number(a) and is_number(b):
+        return a + b
+    if is_number(a) and a == 0:
+        return b
+    if is_number(b) and b == 0:
+        return a
+    return make_sum(a, b)
+
+def eval_diff(a, b):
+    if is_number(a) and is_number(b):
+        return a - b
+    if is_number(a) and a == 0:
+        return -b
+    if is_number(b) and b == 0:
+        return a
+    return make_diff(a, b)
+
+def eval_prod(a, b):
+    if is_number(a) and is_number(b):
+        return a * b
+    if is_number(a) and a == 0:
+        return 0
+    if is_number(b) and b == 0:
+        return 0
+    return make_prod(a, b)
+
+def eval_div(a, b):
+    if is_number(b) and b == 0:
+        raise ZeroDivisionError
+    if is_number(a) and is_number(b):
+        return a / b
+    if is_number(a) and a == 0:
+        return 0
+    return make_div(a, b)
+
+
+
+def eval_exp(e):
+    if is_basic_exp(e):
+        return e
+    op, a, b = e[0], eval_exp(e[1]), eval_exp(e[2])
+    if op == '+':
+        return eval_sum(a, b)
+    elif op == '-':
+        return eval_diff(a, b)
+    elif op == '*':
+        return eval_prod(a, b)
+    elif op == '/':
+        return eval_div(a, b)
+    else:
+        raise ValueError('Unknown operator:', op)
