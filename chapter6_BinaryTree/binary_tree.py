8bb3e21482331e1283d9244452aeffe02324056f
zhe <1655604975@qq.com>
2018-07-05 17:05:14 +0800

    list实现优先队列

--- chapter6_BinaryTree/binary_tree.py
+++ chapter6_BinaryTree/binary_tree.py
@@ -103,3 +103,35 @@ def eval_exp(e):
         return eval_div(a, b)
     else:
         raise ValueError('Unknown operator:', op)
+
+
+class PrioQueueError(ValueError):
+    pass
+
+class PrioQue():
+    def __init__(self, elist=[]):
+        self._elems = list(elist)
+        self._elems.sort(reverse=True)
+
+    def qbqueue(self, e):
+        i = len(self._elems) - 1
+        while i >= 0:
+            if self._elems[i] <= e:
+                i -= 1
+            else:
+                break
+        self._elems.insert(i+1, e)
+
+    def is_empty(self):
+        return not self._elems
+
+    def peek(self):
+        if self.is_empty():
+            raise PrioQueueError
+        return self._elems[-1]
+
+    def dequeue(self):
+        if self.is_empty():
+            raise PrioQueueError
+        return self._elems.pop()
+
