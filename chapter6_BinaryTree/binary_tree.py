aa7a6a4416b50842c82cccb861b4cf46301b949e
zhe <1655604975@qq.com>
2018-07-06 09:48:05 +0800

    实现堆的插入元素

--- chapter6_BinaryTree/binary_tree.py
+++ chapter6_BinaryTree/binary_tree.py
@@ -135,3 +135,28 @@ class PrioQue():
             raise PrioQueueError
         return self._elems.pop()
 
+
+class PrioQueue():
+    def __init__(self, elist=[]):
+        self._elems = list(elist)
+        if elist:
+            self.buildheap()
+
+    def is_empty(self):
+        return not self._elems
+
+    def peek(self):
+        if self.is_empty():
+            raise PrioQueueError
+        return self._elems[0]
+
+    def enqueue(self, e):
+        self._elems.append(None)
+        self.siftup(e, len(self._elems) - 1)
+
+    def siftup(self, e, last):
+        elems, i, j = self._elems, last, (last - 1) // 2
+        while i > 0 and e < elems[j]:
+            elems[i] = elems[j]
+            i, j = j, (j - 1) // 2
+        elems[i] = e
