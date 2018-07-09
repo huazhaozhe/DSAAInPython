ffc4e2c44b7d7e3f30baecbdcbb8f99e39e867ab
zhe <1655604975@qq.com>
2018-07-09 14:36:15 +0800

    选择排序

--- chapter9_Sorting/simple_sorting_algorithm.py
+++ chapter9_Sorting/simple_sorting_algorithm.py
@@ -15,8 +15,22 @@ def insert_sort(lst):
     for i in range(1, len(lst)):
         value = lst[i]
         j = i
-        while j > 0 and lst[j-1] > value:
-            lst[j] = lst[j-1]
+        while j > 0 and lst[j - 1] > value:
+            lst[j] = lst[j - 1]
             j -= 1
         lst[j] = value
 
+
+def select_sort(lst):
+    '''
+    选择排序
+    :param lst: 同insert_sort
+    :return: 同insert_sort
+    '''
+    for i in range(len(lst) - 1):
+        index = i
+        for j in range(i, len(lst)):
+            if lst[j] < lst[index]:
+                index = j
+        if lst[index] < lst[i]:
+            lst[i], lst[index] = lst[index], lst[i]
