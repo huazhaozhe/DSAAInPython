e7dd0025bbbae16b7dc067384c93c35785eba0e6
zhe <1655604975@qq.com>
2018-07-10 09:15:23 +0800

    另一种快速排序算法

--- chapter9_Sorting/quicksort.py
+++ chapter9_Sorting/quicksort.py
@@ -29,3 +29,20 @@ def qucik_sort1(lst):
         qsort_rec(lst, i + 1, r)
 
     qsort_rec(lst, 0, len(lst) - 1)
+
+
+def quick_sort2(lst):
+    def qsort(lst, begin, end):
+        if begin >= end:
+            return
+        pivot = lst[begin]
+        i = begin
+        for index in range(begin + 1, end + 1):
+            if lst[index] < pivot:
+                i += 1
+                lst[i], lst[index] = lst[index], lst[i]
+        lst[begin], lst[i] = lst[i], pivot
+        qsort(lst, begin, i - 1)
+        qsort(lst, i + 1, end)
+
+    qsort(lst, 0, len(lst) - 1)
