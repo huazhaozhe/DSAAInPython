6b02070532e1982d016f73189e6d00fa4da32b78
zhe <1655604975@qq.com>
2018-07-10 17:30:15 +0800

    快速排序实现第三种

--- chapter9_Sorting/quicksort.py
+++ chapter9_Sorting/quicksort.py
@@ -46,3 +46,55 @@ def quick_sort2(lst):
         qsort(lst, i + 1, end)
 
     qsort(lst, 0, len(lst) - 1)
+
+
+def quick_sort3(lst):
+    def qsort(lst, l, r):
+        if l >= r:
+            return
+        i, j = l + 1, r
+        pivot = lst[l]
+        while i < j:
+            while i < j and lst[j] >= pivot:
+                j -= 1
+            while i < j and lst[i] < pivot:
+                i += 1
+            if i < j:
+                # print('change %s:%s %s:%s' % (i, lst[i], j, lst[j]))
+                lst[i], lst[j] = lst[j], lst[i]
+                # print('changed', lst)
+                i += 1
+                j -= 1
+
+        if i == j and lst[i] < pivot:
+            lst[l], lst[i] = lst[i], pivot
+            # print(lst)
+            qsort(lst, l, i - 1)
+            qsort(lst, i + 1, r)
+        else:
+            lst[l], lst[i - 1] = lst[i - 1], pivot
+            # print(lst)
+            qsort(lst, l, i - 2)
+            qsort(lst, i, r)
+
+    qsort(lst, 0, len(lst) - 1)
+
+
+if __name__ == '__main__':
+    from random import randint
+
+    for i in range(10):
+        a = []
+        for j in range(100, randint(1000, 10000)):
+            a.append(randint(0, j))
+        b = a[:]
+        test1 = a[:]
+        test2 = a[:]
+        test3 = a[:]
+        b.sort()
+        qucik_sort1(test1)
+        quick_sort2(test2)
+        quick_sort3(test3)
+        assert test1 == b
+        assert test2 == b
+        assert test3 == b
